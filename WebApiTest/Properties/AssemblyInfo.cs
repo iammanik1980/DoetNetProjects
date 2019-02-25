9375f4dea31fa07d3e1f8480d8df375b7e8d4b33
manikghosh1980 <47991953+manikghosh1980@users.noreply.github.com>
2019-02-25 22:57:39 +0100

    Add files via upload

---
+++ WebApiTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebApiTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IBM")]
+[assembly: AssemblyProduct("WebApiTest")]
+[assembly: AssemblyCopyright("Copyright © IBM 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99c5ae41-2975-4ab5-a7f2-b8ced6090bad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebApiTest/UnitTestClimateAPI.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClimateWebAPI.Controllers;
+using ClimateWebAPI.Models;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace WebApiTest
+{
+    [TestClass]
+    public class UnitTestClimateAPI
+    {
+        [TestMethod]
+        public void GetRecommendation_ShouldBeOK()
+        {
+ 
+            var controller = new ClimateController();
+            var response = controller.Get("2886242") ;
+            var contentResult = response as OkNegotiatedContentResult<RecommendationResponse>;
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNull(contentResult.Content);
+        }
+
+        [TestMethod]
+        public void GetRecommendation_InvalidCityCode()
+        {
+            var controller = new ClimateController();
+            IHttpActionResult response = controller.Get("123");
+            var contentResult = response as OkNegotiatedContentResult<RecommendationResponse>;
+            Assert.IsNull(contentResult);
+          }
+
+        [TestMethod]
+        public void GetRecommendation_MockData_Valid()
+        {
+            var controller = new ClimateController();
+            var response = controller.Get("101");
+            var contentResult = response as OkNegotiatedContentResult<RecommendationResponse>;
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNull(contentResult.Content);
+            Assert.AreEqual(contentResult.Content.Temparature, 22);
+        }
+    }
+}
